1187fa32b0fd4d18863f710f0310fbe9fb0f8d39
Atharva Zalkikar <72726199+AtharvaZalkikar@users.noreply.github.com>
2023-09-27 17:35:12 +0530

    Object literals and different ways to implement it

--- Array_Object_Functions.js
+++ Array_Object_Functions.js
@@ -154,3 +154,128 @@ function letterFinder(word,match){                                    // A funct
 }
 letterFinder('test','t')
 //------------------------------------------------------------------------------------------------------------------------------------
+
+// OBJECTS
+
+//Object literals and the dot notation
+
+/*creating an object with properties and their values
+
+var assistantManager = {                                // <---------- Creating object assistantmanager with literal "{ }" 
+    rangeTilesPerTurn: 3,                         
+    socialSkills: 30,                                          
+    streetSmarts: 30,
+    health: 40,
+    specialAbility: "young and ambitious",
+    greeting: "Let's make some money"
+}
+
+*/
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Notice that it's very easy to build any kind of an object in JavaScript using this example syntax.
+
+//For example, here's a "table" object:
+                                                                        // (---- OBJECT -----)
+var table = {
+    legs: 3,
+    color: "brown",
+    priceUSD: 100,                                  //To access the table object, I can simply console log the entire object:
+}                                               
+console.log(table);                                 //display the object in the developer console  
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/* 
+An alternative approach of building objects is to first save an empty object literal to a variable, 
+then use the dot notation to declare new properties on the fly, and use the assignment operator to add values to those properties; 
+for example:
+*/
+
+var house2 = {};
+house2.rooms = 4;
+house2.color = "pink";
+house2.priceUSD = 12345;
+console.log(house2); // {rooms: 3, color: "brown", priceUSD: 10000}
+                      
+                      //Additionally, nothing is preventing me from combining the two approaches. For example:  
+
+house2.windows = 10;
+console.log(house2); // {rooms: 3, color: "brown", priceUSD: 10000, windows: 10}
+
+//This flexbility additionally means that I can update already existing properties, not just add new ones:  
+
+house2.windows = 11;
+console.log(house2); // {rooms: 3, color: "brown", priceUSD: 10000, windows: 11}
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*  
+
+Brackets Notation
+
+To understand how it works, it's best to use an example, 
+so I'll go through the process of coding the house2 object again, in the same way that I did with the dot notation, only this time, I'll use the brackets notation.
+
+*/
+var house2 = {};
+house2["rooms"] = 4;
+house2['color']= "pink";
+house2["priceUSD"] = 12345;
+console.log(house2); // {rooms: 4, color: 'pink', priceUSD: 12345}
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+
+//I can both access and update properties on objects using either the dot notation, or the brackets notation, or a combination of both, like in the following example:
+
+var car = {};
+car.color = "red";
+car["color"] = "green";
+car["speed"] = 200;
+car.speed = 100;
+console.log(car); // {color: "green", speed: 100}
+
+//With the brackets notation, I can add space characters inside property names, like this:  
+
+car["number of doors"] = 4;
+console.log(car);                     // {color: 'green', speed: 100, number of doors: 5}
+
+//-----------------------------------------------------------------------------------------------------------------
+
+/*
+
+Finally, there's one really useful thing that bracket notation has but is not available in the dot notation: It can evaluate expressions.
+
+To understand what that means, consider the following example:*/
+
+var arrOfKeys = ['speed', 'altitude', 'color'];
+var drone = {
+    speed: 100,
+    altitude: 200,
+    color: "red"
+}
+for (var i = 0; i < arrOfKeys.length; i++) {
+    console.log(drone[arrOfKeys[i]])
+}
+
+/*The above code will result in the following output:  
+100
+200
+red*/
+
+
+/*
+
+Using the fact that brackets notation can evaluate expressions, I accessed the arrOfKeys[i] property on the drone object. 
+
+This value changed on each loop while the for loop was running.
+
+Specifically, the first time it ran, it was evaluated like this: 
+
+The value of i was 0 
+
+The value of arrOfKeys[i] was arrOfKeys[0], which was "speed" 
+
+Thus, drone[arrOfKeys[i]] was evaluated to drone["speed"] which is equal to 100
+
+This allowed me to loop over each of the values stored inside the drone object, based on each of its properties' keys.
+
+*/
