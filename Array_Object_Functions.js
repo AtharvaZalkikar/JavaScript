d79f3d8ea2193f9b9f9e1b36a030a5b5a74d6b40
Atharva Zalkikar <72726199+AtharvaZalkikar@users.noreply.github.com>
2023-09-27 22:12:22 +0530

    Final Update
    Added and exercise related to creating an Array and Object

--- Array_Object_Functions.js
+++ Array_Object_Functions.js
@@ -343,3 +343,47 @@ console.log(simpleArr); // ['apple','pear','plum']
 /* The math object is really useful when using some of its built in methods, one of which is the random method. For example, 
 this method allows you to generate a decimal number between 0 and 0.99.*/
 
+/* Exercise: Creating arrays and objects 
+
+In this exercise lab you will practice creating arrays and objects.
+
+Tasks to complete
+Create a new empty array literal and assign it to the variable clothes.
+
+Add 5 of your favorite items of clothing as strings using the push() method.
+
+Remove the fifth piece of clothing from the array using the pop() method.
+
+Add a new piece of clothing using the push() method.
+
+Use console.log to show the third item from the clothes array in the console.
+
+Create a new empty object literal and assign it to the variable favCar.
+
+Using the dot notation, assign a color property to the favCar object and give it a string value with the color of your choice.
+
+Using the dot notation, assign a covertible property to the favCar object and give it a boolean value of your choice.
+
+Use the console to log the entire favCar object.
+
+*/
+
+var clothes = [];
+clothes.push("shirts");
+clothes.push("Pants");
+clothes.push("Joggers");
+clothes.push("Shorts");
+clothes.push("Vests");
+
+clothes.pop();
+
+clothes.push("drama")
+
+console.log(clothes[2]);
+
+var favCar = {};
+favCar.color = "Red";
+favCar.convertible = false;
+console.log(favCar);
+
+///----------------------------------------------------------------------------------------------------------------------------------
